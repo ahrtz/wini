39c9488c7690d99728f5602e82cb7fd298c8914f
ellen <ellen6666@naver.com>
2020-10-21 15:05:56 +0900

    [Main] doing main page design

--- Front/winism/src/views/Main.vue
+++ Front/winism/src/views/Main.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
 <div class="main-food-area">
 <img src="@/assets/logo/wine-grapes-drink.jpg"/>
  <v-row
@@ -21,11 +22,85 @@
       </v-col>
     </v-row>
 
-
+</div>
 
  <v-card
     class="mx-auto"
-    max-width="344"
+    max-width="500"
+  >
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="display-1 text--primary">
+        be•nev•o•lent
+      </p>
+      <p>adjective</p>
+      <div class="text--primary">
+        well meaning and kindly.<br>
+        "a benevolent smile"
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="deep-purple accent-4"
+      >
+        Learn More
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+<v-card
+    class="mx-auto"
+    max-width="500"
+  >
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="display-1 text--primary">
+        be•nev•o•lent
+      </p>
+      <p>adjective</p>
+      <div class="text--primary">
+        well meaning and kindly.<br>
+        "a benevolent smile"
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="deep-purple accent-4"
+      >
+        Learn More
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+  <v-card
+    class="mx-auto"
+    max-width="500"
+  >
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="display-1 text--primary">
+        be•nev•o•lent
+      </p>
+      <p>adjective</p>
+      <div class="text--primary">
+        well meaning and kindly.<br>
+        "a benevolent smile"
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="deep-purple accent-4"
+      >
+        Learn More
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+  <v-card
+    class="mx-auto"
+    max-width="500"
   >
     <v-card-text>
       <div>Word of the Day</div>
@@ -48,6 +123,7 @@
     </v-card-actions>
   </v-card>
 </div>
+
 </template>
 
 <script>
