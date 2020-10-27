1a8fe5c859c0eedd84d5749640b2685be729250c
ellen <ellen6666@naver.com>
2020-10-27 15:11:11 +0900

    [Guide] succeed to provide framework

--- Front/winism/src/pages/component/Guide.vue
+++ Front/winism/src/pages/component/Guide.vue
@@ -16,10 +16,11 @@
     </div>
 <v-app>
   <v-container class="pt-5">
-  <div  style="margin-top:160px">
+  <div style="margin-top:160px">
      <v-tabs
-    fixed-tabs
+
     background-color="#750049"
+    centered
     dark
   >
     <v-tab>
@@ -28,7 +29,37 @@
     <v-tab>
       For Pro
     </v-tab>
-  
+  <v-tab-item
+      >
+        <v-card flat>
+          <v-card-text>
+            <h2>1. Choose your Wine Type</h2>
+          레드, 화이트, 로제, 스파클링 중에 그날의 기분이나 곁들여 먹는 음식 등을 고려하여 구입할 와인의 종류와 예산을 먼저 정할 필요가 있다.
+          </v-card-text>
+          <v-card-text>
+            <h2>1. Choose your Wine Type</h2>
+          레드, 화이트, 로제, 스파클링 중에 그날의 기분이나 곁들여 먹는 음식 등을 고려하여 구입할 와인의 종류와 예산을 먼저 정할 필요가 있다.
+          </v-card-text>
+           <v-card-text>
+            <h2>1. Choose your Wine Type</h2>
+          레드, 화이트, 로제, 스파클링 중에 그날의 기분이나 곁들여 먹는 음식 등을 고려하여 구입할 와인의 종류와 예산을 먼저 정할 필요가 있다.
+          </v-card-text>
+           <v-card-text>
+            <h2>1. Choose your Wine Type</h2>
+          레드, 화이트, 로제, 스파클링 중에 그날의 기분이나 곁들여 먹는 음식 등을 고려하여 구입할 와인의 종류와 예산을 먼저 정할 필요가 있다.
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item
+      >
+        <v-card flat>
+          <v-card-text>
+            <h2>1. Choose your Wine Type</h2>
+          레드, 화이트, 로제, 스파클링 중에 그날의 기분이나 곁들여 먹는 음식 등을 고려하여 구입할 와인의 종류와 예산을 먼저 정할 필요가 있다.
+          </v-card-text>
+         
+        </v-card>
+      </v-tab-item>
   </v-tabs>
   </div>
     </v-container>
