47e898ba3173ffe32743585a55414edd2457e76f
ellen <ellen6666@naver.com>
2020-10-27 21:10:31 +0900

    [List] adjust grid to list

--- Front/winism/src/pages/component/List.vue
+++ Front/winism/src/pages/component/List.vue
@@ -1,295 +1,300 @@
 <template>
-<div>
-<v-app>
-  <v-container class="pt-5">
-  <v-row style='margin-top:60px'>
-  <v-col cols="12">
-  <v-text-field
-            label="Search Wine"
-            append-outer-icon="mdi-magnify"
-          ></v-text-field>
-  </v-col>
-  </v-row>
-      <div class="row">
-        <div
-         class="col-md-3 col-sm-3 col-xs-12"
-        >
-          <v-card outlined>
-            <v-card-title>Filters</v-card-title>
-            <v-divider></v-divider>
-            <template>
-              <v-treeview :items="items" :open="[1]" :selected-color="'#fff'" activatable open-on-click dense></v-treeview>
-            </template>
-            <v-divider></v-divider>
-            <template>
-              <v-treeview :items="pairings" :open="[1]" :selected-color="'#fff'" activatable open-on-click dense></v-treeview>
-            </template>
-            <v-divider></v-divider>
-            <v-card-title>Price</v-card-title>
-            <v-range-slider
-              v-model="range"
-              :max="max"
-              :min="min"
-              :height="10"
-              class="align-center"
-              dense
-            ></v-range-slider>
-            <v-row class="pa-2" dense>
-              <v-col cols="12" sm="5">
-                <v-text-field
-                  :value="range[0]"
-                  label="Min"
-                  outlined
-                  dense
-                  @change="$set(range, 0, $event)"
-                ></v-text-field>
+  <div>
+    <v-app>
+      <v-container class="pt-5">
+        <v-row style='margin-top:60px'>
+          <v-col cols="12">
+            <v-text-field label="Search Wine" append-outer-icon="mdi-magnify"></v-text-field>
+          </v-col>
+        </v-row>
+        <div class="row">
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <v-card outlined>
+              <v-card-title>Filters</v-card-title>
+              <v-divider></v-divider>
+              <template>
+                <v-treeview :items="items" :open="[1]" :selected-color="'#fff'" activatable open-on-click dense>
+                </v-treeview>
+              </template>
+              <v-divider></v-divider>
+              <template>
+                <v-treeview :items="pairings" :open="[1]" :selected-color="'#fff'" activatable open-on-click dense>
+                </v-treeview>
+              </template>
+              <v-divider></v-divider>
+              <v-card-title>Price</v-card-title>
+              <v-range-slider v-model="range" :max="max" :min="min" :height="10" class="align-center" dense>
+              </v-range-slider>
+              <v-row class="pa-2" dense>
+                <v-col cols="12" sm="5">
+                  <v-text-field :value="range[0]" label="Min" outlined dense @change="$set(range, 0, $event)">
+                  </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="2">
+                  <p class="pt-2 text-center">TO</p>
+                </v-col>
+                <v-col cols="12" sm="5">
+                  <v-text-field :value="range[1]" label="Max" outlined dense @change="$set(range, 1, $event)">
+                  </v-text-field>
+                </v-col>
+              </v-row>
+              <v-divider></v-divider>
+              <v-card-title class="pb-0">Customer Rating</v-card-title>
+              <v-container class="pt-0" fluid>
+                <v-checkbox append-icon="mdi-star" label="4 & above" hide-details dense></v-checkbox>
+                <v-checkbox append-icon="mdi-star" label="3 & above" hide-details dense></v-checkbox>
+                <v-checkbox append-icon="mdi-star" label="2 & above" hide-details dense></v-checkbox>
+                <v-checkbox append-icon="mdi-star" label="1 & above" hide-details dense></v-checkbox>
+              </v-container>
+              <v-divider></v-divider>
+              <v-card-title class="pb-10">Sweetness</v-card-title>
+              <v-slider v-model="sweety" :tick-labels="sweetness" :max="5" thumb-label="always" step="1" thumb-size="25"
+                tick-size="4"></v-slider>
+              <v-card-title class="pb-10">Body</v-card-title>
+              <v-slider v-model="bodyfeel" :tick-labels="body" :max="5" step="1" ticks="always" thumb-label="always"
+                thumb-size="25" tick-size="4"></v-slider>
+              <v-card-title class="pb-10">Acid</v-card-title>
+              <v-slider v-model="acid" :tick-labels="acidity" :max="5" step="1" ticks="always" thumb-label="always"
+                thumb-size="25" tick-size="4"></v-slider>
+              <v-card-title class="pb-10">Tannin</v-card-title>
+              <v-slider v-model="tanninfeel" :tick-labels="tannin" :max="5" step="1" ticks="always" tick-size="4"
+                thumb-label="always" thumb-size="25"></v-slider>
+            </v-card>
+          </div>
+          <div class="col-md-9 col-sm-9 col-xs-12">
+            <v-row dense>
+              <v-col cols="12" sm="8" class="pl-6 pt-6">
+                <small>Showing 1-12 of 200 products</small>
               </v-col>
-              <v-col cols="12" sm="2">
-                <p class="pt-2 text-center">TO</p>
-              </v-col>
-              <v-col cols="12" sm="5">
-                <v-text-field
-                  :value="range[1]"
-                  label="Max"
-                  outlined
-                  dense
-                  @change="$set(range, 1, $event)"
-                ></v-text-field>
+              <v-col cols="12" sm="4">
+                <v-select class="pa-0" v-model="select" :items="options" style="margin-bottom: -20px;" outlined dense>
+                </v-select>
               </v-col>
             </v-row>
+
             <v-divider></v-divider>
-            <v-card-title class="pb-0">Customer Rating</v-card-title>
-            <v-container class="pt-0"  fluid>
-              <v-checkbox append-icon="mdi-star" label="4 & above" hide-details dense></v-checkbox>
-              <v-checkbox append-icon="mdi-star" label="3 & above" hide-details dense></v-checkbox>
-              <v-checkbox append-icon="mdi-star" label="2 & above" hide-details dense></v-checkbox>
-              <v-checkbox append-icon="mdi-star" label="1 & above" hide-details dense></v-checkbox>
-            </v-container>
-            <v-divider></v-divider>
-            <v-card-title class="pb-10">Sweetness</v-card-title>
-            <v-slider
-        v-model="sweety"
-        :tick-labels="sweetness"
-        :max="5"
-        thumb-label="always"
-        step="1"
 
-        tick-size="4"
-      ></v-slider>
-      <v-card-title class="pb-10">Body</v-card-title>
-            <v-slider
-        v-model="bodyfeel"
-        :tick-labels="body"
-        :max="5"
-        step="1"
-        ticks="always"
-        thumb-label="always"
-        tick-size="4"
-      ></v-slider>
-      <v-card-title class="pb-10">Acid</v-card-title>
-            <v-slider
-        v-model="acid"
-        :tick-labels="acidity"
-        :max="5"
-        step="1"
-        ticks="always"
-        thumb-label="always"
-        tick-size="4"
-      ></v-slider>
-      <v-card-title class="pb-10">Tannin</v-card-title>
-            <v-slider
-        v-model="tanninfeel"
-        :tick-labels="tannin"
-        :max="5"
-        step="1"
-        ticks="always"
-        tick-size="4"
-        thumb-label="always"
-      ></v-slider>
-          </v-card>
-        </div>
-        <div
-          class="col-md-9 col-sm-9 col-xs-12"
-        >
-          <v-row dense>
-            <v-col cols="12" sm="8" class="pl-6 pt-6">
-              <small>Showing 1-12 of 200 products</small>
-            </v-col>
-            <v-col cols="12" sm="4">
-              <v-select class="pa-0" v-model="select" :items="options" style="margin-bottom: -20px;" outlined dense></v-select>
-            </v-col>
-          </v-row>
+            <div class="row text-center">
+              <div class="col-12" :key="pro.id" v-for="pro in products">
+                <v-hover v-slot:default="{ hover }">
+                  <v-card class="mx-auto v-flex" color="grey lighten-4" max-width="600">
 
-          <v-divider></v-divider>
-
-          <div class="row text-center">
-            <div class="col-12" :key="pro.id" v-for="pro in products">
-              <v-hover v-slot:default="{ hover }">
-                <v-card
-                  class="mx-auto"
-                  color="grey lighten-4"
-                  max-width="600"
-                >
-                  <v-img
-                    class="white--text align-end"
-                    :aspect-ratio="16/9"
-                    height="200px"
-                    :src="pro.src"
-                  >
-                    <v-card-title>{{pro.type}} </v-card-title>
-                    <v-expand-transition>
-                      <div
-                        v-if="hover"
-                        class="d-flex transition-fast-in-fast-out white darken-2 v-card--reveal display-3 white--text"
-                        style="height: 100%;"
-                      >
-                        <v-btn v-if="hover" href="/product" class="" outlined>VIEW</v-btn>
+  
+                      <div>
+                        <v-img :aspect-ratio="16/9" height="200px" :src="pro.src">
+                        </v-img>
+                        <v-card-title>{{pro.type}} </v-card-title>
                       </div>
 
-                    </v-expand-transition>
-                  </v-img>
-                    <v-slider
-      v-model="ex1.val"
-      :color="ex1.color"
-      :label="ex1.label"
-      :readonly="true"
-    ></v-slider>
-    <template>
-  <slider class="slider-success"
-             v-model="simple" :disabled="true" :label="ex1.label">
-  </slider>
-</template>
-                  <v-card-text class="text--primary">
-                    <div><a href="/product" style="text-decoration: none">{{pro.name}}</a></div>
-                    <div>${{pro.price}}</div>
-                  </v-card-text>
-                </v-card>
-              </v-hover>
+                      <v-expand-transition>
+                        <div v-if="hover"
+                          class="d-flex transition-fast-in-fast-out white darken-2 v-card--reveal display-3 white--text"
+                          style="height: 100%;">
+                          <v-btn v-if="hover" href="/detail" class="" outlined>VIEW</v-btn>
+                        </div>
+
+                      </v-expand-transition>
+
+
+
+                      <v-slider v-model="ex1.val" :color="ex1.color" :label="ex1.label" :readonly="true"></v-slider>
+                      <template>
+                        sweety<slider class="slider-success" v-model="simple" :disabled="false" :value="ex1.label">10
+                        </slider>
+                      </template>
+                      <v-card-text class="text--primary">
+                        <div><a href="/product" style="text-decoration: none">{{pro.name}}</a></div>
+                        <div>${{pro.price}}</div>
+                      </v-card-text>
+
+                  </v-card>
+                </v-hover>
+              </div>
+            </div>
+<v-card>
+      <v-container
+        fluid
+        grid-list-lg
+      >
+        <v-layout row wrap>
+        <v-flex xs12>
+            <v-card color="grey lighten-4">
+              <v-layout>
+                <v-flex xs5>
+                  <v-img
+                    src="../../../public/img/wine-bottle-grapes.jpg"
+                    height="125px"
+                    contain
+                  ></v-img>
+                </v-flex>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">Supermodel</div>
+                      <div>Foster the People</div>
+                      <div>(2014)</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+             
+            </v-card>
+          </v-flex>
+
+        </v-layout>
+      </v-container>
+</v-card>
+            <div class="text-center mt-12">
+              <v-pagination v-model="page" :length="6"></v-pagination>
             </div>
           </div>
-          
-          <div class="text-center mt-12">
-            <v-pagination
-              v-model="page"
-              :length="6"
-            ></v-pagination>
-          </div>
         </div>
-      </div>
-    </v-container>
+      </v-container>
 
- 
-  </v-app>
+
+    </v-app>
   </div>
 
 </template>
 <script>
-import {Slider} from '@/components'
-export default {
-  name: 'List',
-  components: {
- Slider
-  },
-  data(){
-      return{
-        //wine list slider data
-        simple:30,
-        //wine sweety
-        sweety:0,
-        bodyfeel:0,
-        acid:0,
-        tanninfeel:0,
+  import {
+    Slider
+  } from '@/components'
 
-            range: [0, 10000],
-            select:'Popularity',
-            options: [
-                'Default',
-                'Popularity',
-                'Relevance',
-                'Price: Low to High',
-                'Price: High to Low',
+  export default {
+    name: 'List',
+    components: {
+      [Slider.name]: Slider
+    },
+    data() {
+      return {
+        //wine list slider data
+        simple: 30,
+        //wine sweety
+        sweety: 0,
+        bodyfeel: 0,
+        acid: 0,
+        tanninfeel: 0,
+
+        range: [0, 10000],
+        select: 'Popularity',
+        options: [
+          'Default',
+          'Popularity',
+          'Relevance',
+          'Price: Low to High',
+          'Price: High to Low',
+        ],
+        page: 1,
+        breadcrums: [{
+            text: 'Home',
+            disabled: false,
+            href: 'breadcrumbs_home',
+          },
+          {
+            text: 'Type',
+            disabled: false,
+            href: 'breadcrumbs_type',
+          },
+          {
+            text: 'Red',
+            disabled: true,
+            href: 'breadcrumbs_red',
+          },
+        ],
+        min: 0,
+        max: 10000,
+        items: [{
+            id: 2,
+            name: 'Type',
+            children: [{
+                id: 2,
+                name: 'Red'
+              },
+              {
+                id: 3,
+                name: 'White'
+              },
+              {
+                id: 4,
+                name: 'Rose'
+              },
+              {
+                id: 5,
+                name: 'Sparkling'
+              },
             ],
-            page:1,
-            breadcrums: [
-                {
-                    text: 'Home',
-                    disabled: false,
-                    href: 'breadcrumbs_home',
-                },
-                {
-                    text: 'Type',
-                    disabled: false,
-                    href: 'breadcrumbs_type',
-                },
-                {
-                    text: 'Red',
-                    disabled: true,
-                    href: 'breadcrumbs_red',
-                },
+          },
+
+        ],
+        pairings: [{
+            id: 2,
+            name: 'Pairing',
+            children: [{
+                id: 2,
+                name: 'Beef'
+              },
+              {
+                id: 3,
+                name: 'Rich fish'
+              },
+              {
+                id: 4,
+                name: 'Lamb'
+              },
+              {
+                id: 5,
+                name: 'Vegetarian'
+              },
+              {
+                id: 6,
+                name: 'Pasta'
+              },
+              {
+                id: 7,
+                name: 'Pork'
+              },
             ],
-            min:0,
-            max:10000,
-            items: [
-                {
-                    id: 2,
-                    name: 'Type',
-                    children: [
-                        { id: 2, name: 'Red' },
-                        { id: 3, name: 'White' },
-                        { id: 4, name: 'Rose' },
-                        { id: 5, name: 'Sparkling' },
-                    ],
-                },
-           
-            ],
-            pairings: [
-                {
-                    id: 2,
-                    name: 'Pairing',
-                    children: [
-                        { id: 2, name: 'Beef' },
-                        { id: 3, name: 'Rich fish' },
-                        { id: 4, name: 'Lamb' },
-                        { id: 5, name: 'Vegetarian' },
-                        { id: 6, name: 'Pasta' },
-                        { id: 7, name: 'Pork' },
-                    ],
-                },
-           
-            ],
-              ex1: { label: 'sweety', val: 25, color: '#750049'},
-            products:[
-                {
-                    id:1,
-                    name:'Alvaro Palacios Villa de Corullon',
-                    type:'Red',
-                    price:'18.00',
-                    src:require('../../../public/img/wine-bottle-grapes.jpg')
-                },
-                
-               
-            ],
-       
-        sweetness:[
+          },
+
+        ],
+        ex1: {
+          label: 'sweety',
+          val: 25,
+          color: '#750049'
+        },
+        products: [{
+            id: 1,
+            name: 'Alvaro Palacios Villa de Corullon',
+            type: 'Red',
+            price: '18.00',
+            src: require('../../../public/img/wine-bottle-grapes.jpg')
+          },
+
+
+        ],
+
+        sweetness: [
           'Dry',
-          '','','','','Sweet'
+          '', '', '', '', 'Sweet'
         ],
-        body:[
+        body: [
           'Light',
-          '','','','','Full'
+          '', '', '', '', 'Full'
         ],
-        
-        acidity:[
+
+        acidity: [
           'Low',
-          '','','','','High'
+          '', '', '', '', 'High'
         ],
-        tannin:[
+        tannin: [
           'Low',
-          '','','','','High'
+          '', '', '', '', 'High'
         ]
       }
-  }
-};
+    }
+  };
 </script>
 <style scoped>
   .v-card--reveal {
@@ -300,11 +305,5 @@ export default {
     position: absolute;
     width: 100%;
   }
-.v-slider--horizontal .v-slider__track-container{
-  width:100%;
-  height:20px;
-  left:0;
-  top:50%;
-  transform:translateY(-50%)
-  }
+
 </style>
