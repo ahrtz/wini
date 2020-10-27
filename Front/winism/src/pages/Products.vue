ae5dd63a5ec0e49c9a412992074e8960181b9f6c
ahrtz <ahrtzzinn@gmail.com>
2020-10-27 13:46:00 +0900

    [mod]상품상세

--- Front/winism/src/pages/Products.vue
+++ Front/winism/src/pages/Products.vue
@@ -34,30 +34,23 @@
           </v-carousel>
         </div>
         <div class="col-md-7 col-sm-12 col-xs-12">
-          <v-breadcrumbs class="pb-0" :items="item"></v-breadcrumbs>
           <div class="pl-6">
             <p class="display-1 mb-0">Modern Black T-Shirt</p>
             <v-card-actions class="pa-0">
-              <p class="headline font-weight-light pt-3">$65.00 <del style="" class="subtitle-1 font-weight-thin">$80.00</del></p>
               <v-spacer></v-spacer>
               <!-- <v-rating v-model="rating" class="" background-color="warning lighten-3"
                         color="warning" dense></v-rating> -->
-              <span class="body-2	font-weight-thin"> 25 REVIEWS</span>
+              <span class="body-2	font-weight-thin"> 앞에 별점? 25 REVIEWS</span>
             </v-card-actions>
-            <p class="subtitle-1 font-weight-thin">
+            <!-- <p class="subtitle-1 font-weight-thin">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Tincidunt arcu non sodales neque sodales ut etiam. Lectus arcu bibendum at varius vel pharetra. Morbi tristique senectus et netus et malesuada.
-            </p>
-            <p class="title">SIZE</p>
+            </p> -->
+            <!-- <p class="title">SIZE</p>
             
-            <p class="title">ITEMS</p>
+            <p class="title">ITEMS</p> -->
 
-            <v-text-field
-                outlined
-                style="width:100px"
-                :value="1"
-                dense
-            ></v-text-field>
-            <v-btn class="primary white--text" outlined tile dense><v-icon>mdi-cart</v-icon> 리뷰 남기기</v-btn>
+            
+            <v-btn class="primary white--text " outlined tile dense><v-icon>mdi-cart</v-icon> 리뷰 남기기</v-btn>
             <v-btn class="ml-4" outlined tile>ADD TO WISHLIST</v-btn>
 
           </div>
@@ -85,27 +78,17 @@
               </p>
             </v-tab-item>
             <v-tab-item>
-              <v-list
-                three-line="true"
-                avatar="true"
-              >
-                <v-list-item-group v-model="item" color="primary">
-                  <v-list-item
-                    v-for="(item, i) in items"
-                    :key="i"
-                    inactive="true"
-                  >
-                    <v-list-item-avatar>
-                      <v-img :src="item.avatar"></v-img>
-                    </v-list-item-avatar>
-                    <v-list-item-content>
-                      <v-list-item-title v-html="item.title"></v-list-item-title><v-rating v-model="rating" class="" background-color="warning lighten-3"
-                                                                                           color="warning" dense></v-rating>
-                      <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list-item-group>
-              </v-list>
+              <v-card v-for="review in reviews" :key="review.reviewid" class="row">
+                  
+                  <v-card-title>{{review.content}}</v-card-title>
+                  <v-spacer></v-spacer>
+                  <v-rating v-model="review.rate" readonly></v-rating>
+                  
+                  <v-card-text>{{review.content}}</v-card-text>
+                  
+              </v-card>
+              
+              
             </v-tab-item>
           </v-tabs>
           <v-card-text
@@ -116,62 +99,31 @@
             <p class="subtitle-1 font-weight-light pt-3 text-center">YOU MIGHT ALSO LIKE</p>
             <v-divider></v-divider>
             <div class="row text-center">
-              <div class="col-md-2 col-sm-4 col-xs-12 text-center">
+              <div class="col-md-3 col-sm-6 col-xs-12 text-center" v-for="item in recommend" :key="item.itemid">
                 <v-hover
                   v-slot:default="{ hover }"
                   open-delay="200"
                 >
                   <v-card
                     :elevation="hover ? 16 : 2"
+                    
                   >
                     <v-img
-                      class="white--text align-end"
-                      height="200px"
+                      class="white--text align-center col-md-12 col-xs-12"
+                      
                       src="img/hero-image-2.png"
                       
-                    >
-                    </v-img>
-
-                      <v-card-title>와인이름이 겁나 긴게 백퍼센트 있을텐데 그럼 이거는 어떻게 보여지는거지 </v-card-title>
-                    <v-card-text class="text--primary text-center">
-                      <div>Upto 60% + Extra 10%</div>
-                      <div>Baggit, Zara, Fossil</div>
-                    </v-card-text>
-
-                    <div class="text-center">
-                      <v-btn
-                        class="ma-2"
-                        outlined
-                        color="info"
-                      >
-                        Explore
-                      </v-btn>
-                    </div>
-                  </v-card>
-                </v-hover>
-              </div>
-              <!-- <div class="col-md-2 col-sm-4 col-xs-12 text-center" >
-                <v-hover
-                  v-slot:default="{ hover }"
-                  open-delay="200"
-                >
-                  <v-card
-                    :elevation="hover ? 16 : 2"
-                  >
-                    <v-img
-                      class="white--text align-end"
-                      height="200px"
                       
                     >
-                      <v-card-title>T-Shirt </v-card-title>
                     </v-img>
-
+                    <div class="col-md-12 col-xs-12">
+                      <h6 class="py-0 ">{{item.itemname}} </h6>
                     <v-card-text class="text--primary text-center">
-                      <div>Upto 50%</div>
-                      <div>Zara, Selected, Celio</div>
+                      <div>기타등등 정보</div>
+                      <div>이건 기타등등 기타 등등 기타 등등의 정보가 옵니다</div>
                     </v-card-text>
-
-                    <div class="text-center">
+                    </div>
+                    <div class="text-center col-12">
                       <v-btn
                         class="ma-2"
                         outlined
@@ -183,138 +135,7 @@
                   </v-card>
                 </v-hover>
               </div>
-              <div class="col-md-2 col-sm-4 col-xs-12 text-center" >
-                <v-hover
-                  v-slot:default="{ hover }"
-                  open-delay="200"
-                >
-                  <v-card
-                    :elevation="hover ? 16 : 2"
-                  >
-                    <v-img
-                      class="white--text align-end"
-                      height="200px"
-                      
-                    >
-                      <v-card-title>Jeans </v-card-title>
-                    </v-img>
 
-                    <v-card-text class="text--primary text-center">
-                      <div>Upto 60% + Extra 10%</div>
-                      <div>Jack & Jones, Levis</div>
-                    </v-card-text>
-
-                    <div class="text-center">
-                      <v-btn
-                        class="ma-2"
-                        outlined
-                        color="info"
-                      >
-                        Explore
-                      </v-btn>
-                    </div>
-                  </v-card>
-                </v-hover>
-              </div>
-              <div class="col-md-2 col-sm-4 col-xs-12 text-center" >
-                <v-hover
-                  v-slot:default="{ hover }"
-                  open-delay="200"
-                >
-                  <v-card
-                    :elevation="hover ? 16 : 2"
-                  >
-                    <v-img
-                      class="white--text align-end"
-                      height="200px"
-                      
-                    >
-                      <v-card-title>Shirts </v-card-title>
-                    </v-img>
-
-                    <v-card-text class="text--primary text-center">
-                      <div>Upto 60% + Extra 10%</div>
-                      <div>Nike, Adidas, Puma</div>
-                    </v-card-text>
-
-                    <div class="text-center">
-                      <v-btn
-                        class="ma-2"
-                        outlined
-                        color="info"
-                      >
-                        Explore
-                      </v-btn>
-                    </div>
-                  </v-card>
-                </v-hover>
-              </div>
-              <div class="col-md-2 col-sm-4 col-xs-12 text-center" >
-                <v-hover
-                  v-slot:default="{ hover }"
-                  open-delay="200"
-                >
-                  <v-card
-                    :elevation="hover ? 16 : 2"
-                  >
-                    <v-img
-                      class="white--text align-end"
-                      height="200px"
-                      
-                    >
-                      <v-card-title>Shoes </v-card-title>
-                    </v-img>
-
-                    <v-card-text class="text--primary text-center">
-                      <div>Upto 60% + Extra 10%</div>
-                      <div>Nike, Adidas, Puma</div>
-                    </v-card-text>
-
-                    <div class="text-center">
-                      <v-btn
-                        class="ma-2"
-                        outlined
-                        color="info"
-                      >
-                        Explore
-                      </v-btn>
-                    </div>
-                  </v-card>
-                </v-hover>
-              </div>
-              <div class="col-md-2 col-sm-4 col-xs-12 text-center" >
-                <v-hover
-                  v-slot:default="{ hover }"
-                  open-delay="200"
-                >
-                  <v-card
-                    :elevation="hover ? 16 : 2"
-                  >
-                    <v-img
-                      class="white--text align-end"
-                      height="200px"
-                     
-                    >
-                      <v-card-title>Jackets </v-card-title>
-                    </v-img>
-
-                    <v-card-text class="text--primary text-center">
-                      <div>Upto 60% + Extra 10%</div>
-                      <div>Nike, Adidas, Puma</div>
-                    </v-card-text>
-
-                    <div class="text-center">
-                      <v-btn
-                        class="ma-2"
-                        outlined
-                        color="info"
-                      >
-                        Explore
-                      </v-btn>
-                    </div>
-                  </v-card>
-                </v-hover>
-              </div> -->
             </div>
           </v-card-text>
         </div>
@@ -350,8 +171,25 @@ export default {
         recommend:[
           {
            itemid:1,
-           itemname:'까르비네 쇼비뇽' 
-          }
+           itemname:'my name is pakr jin soo' 
+          },
+          {
+           itemid:2,
+           itemname:'your name is yoo se jung carbine sobinoung' 
+          },
+        ],
+        reviews:[
+          {
+            reviewid:1,
+            content:"내용1 이게 길어지면 어떻게 될 것인가 낭는 정말 잘 모르겟구나 ",
+            rate:3,
+            
+          },
+          {
+            reviewid:2,
+            content:'내용2',
+            rate:4
+          },
         ]
         }
     }
