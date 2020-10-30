a81f01eba778779754e56e1043c88d18e5794b2a
ellen <ellen6666@naver.com>
2020-10-30 18:51:36 +0900

    [Guide] done with red wine

--- Front/winism/src/pages/component/Guide.vue
+++ Front/winism/src/pages/component/Guide.vue
@@ -39,44 +39,115 @@
       >
       <p>와인 종류에 따른 포도 품종을 선택해보세요!</p>
       <v-row>
-      <v-col cols="12" sm="6">
+      <v-col cols="12">
       <v-card
     class="mx-auto"
     max-width="344"
   >
-    <v-img
-      src="../../../public/img/pngwing.com.png"
 
-    ></v-img>
- <v-card-actions>
-   
 
-      <v-spacer></v-spacer>
-    <v-card-title>
-      Red Wine
-    </v-card-title>
+ <v-menu transition="scroll-x-transition">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="#750049"
+          class="ma-2"
+          v-bind="attrs"
+          v-on="on"
+        >
+          Red Wine
+        </v-btn>
+      </template>
+        <v-tabs vertical>
+      <v-tab>
+        까베르네 쇼비뇽
+      </v-tab>
+      <v-tab>
 
-    
+        말벡
+      </v-tab>
+      <v-tab>
+        
+        까르미네르
+      </v-tab>
+ <v-tab>
+        
+        멜롯
+      </v-tab>
+       <v-tab>
+        
+        쉬라즈
+      </v-tab>
+       <v-tab>
+        
+        피노누아
+      </v-tab>
+       <v-tab>
+        
+        산지오베제
+      </v-tab>
+       <v-tab>
+        
+        템쁘라니요
+      </v-tab>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <p>
+              Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi ac felis. Etiam feugiat lorem non metus. Sed a libero.
+            </p>
 
-   
+            <p>
+              Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non nisl sit amet velit hendrerit rutrum.
+            </p>
 
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
+            <p class="mb-0">
+              Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Phasellus blandit leo ut odio.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <p>
+              Morbi nec metus. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Nunc sed turpis.
+            </p>
 
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
+            <p>
+              Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In hac habitasse platea dictumst. Fusce ac felis sit amet ligula pharetra condimentum.
+            </p>
 
-        <v-card-text>
-         
-        </v-card-text>
-      </div>
-    </v-expand-transition>
+            <p>
+              Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Nam commodo suscipit quam. In consectetuer turpis ut velit. Sed cursus turpis vitae tortor. Aliquam eu nunc.
+            </p>
+
+            <p>
+              Etiam ut purus mattis mauris sodales aliquam. Ut varius tincidunt libero. Aenean viverra rhoncus pede. Duis leo. Fusce fermentum odio nec arcu.
+            </p>
+
+            <p class="mb-0">
+              Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce commodo aliquam arcu. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <p>
+              Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor auctor, justo.
+            </p>
+
+            <p class="mb-0">
+              Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+    </v-menu>
+
+  
   </v-card>
   </v-col>
    <v-col cols="12" sm="6">
@@ -85,7 +156,7 @@
     max-width="344"
   >
     <v-img
-      src="../../../public/img/pngwing.com.png"
+      src="../../../public/img/pngwing.com (1).png"
 
     ></v-img>
  <v-card-actions>
@@ -93,7 +164,7 @@
 
       <v-spacer></v-spacer>
     <v-card-title>
-      Red Wine
+      White Wine
     </v-card-title>
 
     
