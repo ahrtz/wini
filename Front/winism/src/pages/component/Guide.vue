daa2a25d6037fa3af64eec36e34fa1216aba4e2f
ellen <ellen6666@naver.com>
2020-11-01 23:12:30 +0900

    [Guide] tab component

--- Front/winism/src/pages/component/Guide.vue
+++ Front/winism/src/pages/component/Guide.vue
@@ -39,22 +39,112 @@
       >
       <p>와인 종류에 따른 포도 품종을 선택해보세요!</p>
       <v-row>
-      <v-col cols="12">
-      <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-
-
- <v-menu transition="scroll-x-transition">
+      <v-col cols="6">
+ <v-menu transition="scroll-x-transition" :close-on-content-click="false">
       <template v-slot:activator="{ on, attrs }">
         <v-btn
           color="#750049"
-          class="ma-2"
           v-bind="attrs"
           v-on="on"
+          dark
         >
           Red Wine
+        </v-btn>
+      </template>
+        <v-tabs vertical >
+      <v-tab>
+        까베르네 쇼비뇽
+      </v-tab>
+      <v-tab>
+
+        말벡
+      </v-tab>
+      <v-tab>
+        
+        까르미네르
+      </v-tab>
+ <v-tab>
+        
+        멜롯
+      </v-tab>
+       <v-tab>
+        
+        쉬라즈
+      </v-tab>
+       <v-tab>
+        
+        피노누아
+      </v-tab>
+       <v-tab>
+        
+        산지오베제
+      </v-tab>
+       <v-tab>
+        
+        템쁘라니요
+      </v-tab>
+      <v-tab-item>
+
+          <v-card-text>
+            <p>
+              Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi ac felis. Etiam feugiat lorem non metus. Sed a libero.
+            </p>
+
+          
+          </v-card-text>
+  
+      </v-tab-item>
+      <v-tab-item >
+        <v-card flat>
+          <v-card-text>
+            <p>
+              Morbi nec metus. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Nunc sed turpis.
+            </p>
+
+            <p>
+              Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In hac habitasse platea dictumst. Fusce ac felis sit amet ligula pharetra condimentum.
+            </p>
+
+            <p>
+              Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Nam commodo suscipit quam. In consectetuer turpis ut velit. Sed cursus turpis vitae tortor. Aliquam eu nunc.
+            </p>
+
+            <p>
+              Etiam ut purus mattis mauris sodales aliquam. Ut varius tincidunt libero. Aenean viverra rhoncus pede. Duis leo. Fusce fermentum odio nec arcu.
+            </p>
+
+            <p class="mb-0">
+              Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce commodo aliquam arcu. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <p>
+              Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor auctor, justo.
+            </p>
+
+            <p class="mb-0">
+              Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+    </v-menu>
+      </v-col>
+       <v-col cols="6">
+<v-menu transition="scroll-x-transition" :close-on-content-click="false">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="#750049"
+          v-bind="attrs"
+          v-on="on"
+          dark
+        >
+          White Wine
         </v-btn>
       </template>
         <v-tabs vertical>
@@ -94,14 +184,6 @@
           <v-card-text>
             <p>
               Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi ac felis. Etiam feugiat lorem non metus. Sed a libero.
-            </p>
-
-            <p>
-              Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non nisl sit amet velit hendrerit rutrum.
-            </p>
-
-            <p class="mb-0">
-              Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Phasellus blandit leo ut odio.
             </p>
           </v-card-text>
         </v-card>
@@ -146,50 +228,9 @@
       </v-tab-item>
     </v-tabs>
     </v-menu>
+       </v-col>
 
   
-  </v-card>
-  </v-col>
-   <v-col cols="12" sm="6">
-      <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-img
-      src="../../../public/img/pngwing.com (1).png"
-
-    ></v-img>
- <v-card-actions>
-   
-
-      <v-spacer></v-spacer>
-    <v-card-title>
-      White Wine
-    </v-card-title>
-
-    
-
-   
-
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
-
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
-
-        <v-card-text>
-         
-        </v-card-text>
-      </div>
-    </v-expand-transition>
-  </v-card>
-  </v-col>
   </v-row>
         <v-card flat>
          
