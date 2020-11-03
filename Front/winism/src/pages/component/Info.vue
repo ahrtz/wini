b0162c0b53ab90dc868760494e61c7b9585d1ab0
ellen <ellen6666@naver.com>
2020-11-03 16:54:56 +0900

    [Info] doing how to read label design

--- Front/winism/src/pages/component/Info.vue
+++ Front/winism/src/pages/component/Info.vue
@@ -91,7 +91,7 @@
      </v-row>
   
       </v-tab-item>
-    <v-tab-item
+    <v-tab-item class="ma-3"
       >
         <v-hover v-slot="{ hover }">
     <v-card
@@ -101,7 +101,7 @@
     >
       <v-img
         :aspect-ratio="16/9"
-        src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
+        src="../../../public/img/winelabel_france.png"
       >
         <v-expand-transition>
           <div
@@ -117,17 +117,7 @@
         class="pt-6"
         style="position: relative;"
       >
-        <v-btn
-          absolute
-          color="orange"
-          class="white--text"
-          fab
-          large
-          right
-          top
-        >
-          <v-icon>mdi-cart</v-icon>
-        </v-btn>
+        
         <div class="font-weight-light grey--text title mb-2">
           For the perfect meal
         </div>
@@ -140,6 +130,44 @@
         </div>
       </v-card-text>
     </v-card>
+  </v-hover>
+  <v-hover v-slot="{ hover }">
+    <v-list
+      class="mx-auto"
+      color="grey lighten-4"
+      max-width="600"
+    >
+      <v-list-item
+        
+      >
+      <p>우와</p>
+        <v-expand-transition>
+          <div
+            v-if="hover"
+            class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+            style="height: 100%;"
+          >
+            $14.99
+          </div>
+        </v-expand-transition>
+      </v-list-item>
+      <v-card-text
+        class="pt-6"
+        style="position: relative;"
+      >
+        
+        <div class="font-weight-light grey--text title mb-2">
+          For the perfect meal
+        </div>
+        <h3 class="display-1 font-weight-light orange--text mb-2">
+          QW cooking utensils
+        </h3>
+        <div class="font-weight-light title mb-2">
+          Our Vintage kitchen utensils delight any chef.<br>
+          Made of bamboo by hand
+        </div>
+      </v-card-text>
+    </v-list>
   </v-hover>
       </v-tab-item>
     </v-tabs>
@@ -164,64 +192,7 @@ export default {
         show: false,
         tab: null,
         expand: false,
-        items: [
-        {
-          color: 'red lighten-2',
-          icon: 'mdi-star',
-          title:'Choose your Wine Type',
-          content:'먼저 자신이 화이트, 로제, 레드, 스파클링 중 어떤 와인을 원하는지 정하세요.'
-        },
-        {
-          color: 'purple darken-1',
-          icon: 'mdi-book-variant',
-          title:'Choose your Pairing',
-          content:'와인과 곁들일 음식 종류를 선택하세요. 음식 종류에 따라 어울리는 와인이 달라집니다.'
-        },
-        {
-          color: 'green lighten-1',
-          icon: 'mdi-airballoon',
-          title:'Choose your Price',
-          content:'가격대를 정하세요. 자신의 예산에 맞추어 그에 맞는 와인을 선택하세요.'
-        },
-        {
-          color: 'indigo',
-          icon: 'mdi-buffer',
-          title:'Choose your Wine Type',
-          content:'먼저 자신이 화이트, 로제, 레드, 스파클링 중 어떤 와인을 원하는지 정하세요.'
-        },
-      ],
-      beginners: [
-        {
-          color: 'red lighten-2',
-          icon: 'mdi-star',
-          title:'달콤한 화이트 와인',
-          content:'독일 모젤지방의 리슬링 와인종류와 같은 달콤한 화이트 와인을 먼저 도전해보세요. 향기로운 과일 향에 매료되면서 맛있다고 생각하게 될 것입니다.'
-        },
-        {
-          color: 'purple darken-1',
-          icon: 'mdi-book-variant',
-          title:'약간 드라이한 화이트 와인',
-          content:'캘리포니아산 샤도네 혹은 호주산 샤도네를 즐겨보세요. 단맛을 싫어한다면 이 와인부터 시작해도 좋습니다.'
-        },
-        {
-          color: 'green lighten-1',
-          icon: 'mdi-airballoon',
-          title:'떫지 않은 가벼운 레드 와인',
-          content:'프랑스 버건디 지역 특히 보졸레 지방에서 생산되는 와인들이나 이태리 와인들이 좋스비다.'
-        },
-        {
-          color: 'indigo',
-          icon: 'mdi-buffer',
-          title:'부드럽고 약간 진하면서도 과일향이 풍부한 레드 와인',
-          content:'호주산 쉬라즈나 까베르네 소비뇽, 미국산 진판델이나 멜로종류 혹은 멜로 포도품종이 많이 들어간 프랑스의 쌩떼밀리용지역에서 생산되는 와인을 시도해보세요.'
-        },
-         {
-          color: 'pink',
-          icon: 'mdi-glass-tulip',
-          title:'타닌 맛이 강한 와인',
-          content:'프랑스 보르도 와인이나 까베르네 소비뇽과 같은 포도 품종이 많이 사용된 와인들을 접해보세요. 이런 종류의 와인에 익숙해지게 된다면 you are not beginner anymore'
-        },
-      ],
+       
       }
   }
 };
