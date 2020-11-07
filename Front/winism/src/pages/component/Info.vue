1e66a79e12e381ad19b43561dc255a267a3ce992
ellen <ellen6666@naver.com>
2020-11-07 15:36:32 +0900

    [Info] red wine grape finish

--- Front/winism/src/pages/component/Info.vue
+++ Front/winism/src/pages/component/Info.vue
@@ -46,7 +46,7 @@
       </v-btn>
       <modal :show.sync="modals.cabernet" headerClasses="justify-content-center">
         <h4 slot="header" class="title title-up">Cabernet Sauvignon</h4>
-         <strong>떫고, 농도감 있음</strong>
+         <strong>떫고, 농도감이 있는 맛</strong>
           <p>
               와인을 만들 때 가장 많이 사용하는 포도 품종이 바로 까베르네 소비뇽이다. 
               초심자의 입장에서 너무 드라이하거나 떫지 않고 또 너무 묽지도 않은 우리가 생각하는 스탠더드를 찾고자 한다면 까베르네 소비뇽을 선택하면 된다.
@@ -98,14 +98,14 @@
       </v-btn>
       <modal :show.sync="modals.Carmenere" headerClasses="justify-content-center">
         <h4 slot="header" class="title title-up">Carmenere</h4>
-         <strong>적당한 농도감과 부드러움 </strong>
+         <strong>적당한 농도감과 부드러운 맛</strong>
           <p>
               프랑스 보르도 메독이 원산지이며, 까베르네계열의 품종이다. 필록세라 병이 유럽 거의 모든포도원을 파괴하고 난 이후, 오늘날 프랑스에서 까르메네르 품종을 발견하는 것은 거의 불가능하다.
 
 가장 널리 재배되고 있는 곳은 남미 칠레이며 주로 품종 와인을 생산한다.
 
 깊은 레드 색상과 붉은 과일에서 발견되는 아로마, 방향성, 베리향을 가진다.
- 탄닌은 까베르네 소비뇽보다 더 순하고 부드럽고, 와인은 미디엄바디이다.
+ 탄닌은 까베르네 소비뇽보다 더 순하고 부드럽고, 와인은 미디엄 바디이다.
 
 숙성이 잘 되었을 때 체리향과 신선한 과일향, 탄내, 향신료와 흙내음, 깊은 심홍색의 와인이 된다.
 
@@ -118,145 +118,217 @@
         </template>
       </modal>
     </div>
- <v-menu offset-y :close-on-content-click="false">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="#750049"
-          v-bind="attrs"
-          v-on="on"
-          dark
-        >
-          Red Wine
-        </v-btn>
-      </template>
-        <v-tabs vertical >
-      
-      <v-tab>
-        
-        까르메네르(Carmenere)
-      </v-tab>
- <v-tab>
-        
-        멜롯
-      </v-tab>
-       <v-tab>
-        
-        쉬라즈
-      </v-tab>
-       <v-tab>
-        
-        피노누아
-      </v-tab>
-       <v-tab>
-        
-        산지오베제
-      </v-tab>
-       <v-tab>
-        
-        템쁘라니요
-      </v-tab>
-      
-      <v-tab-item >
-        <v-card flat>
-          <v-card-text>
-            <p>
-             떫고 진하고 풍부한 맛
-            </p>
+    <div>
+      <v-btn   color="#750049" dark @click.native="modals.Merlot = true">
+       멜롯(Merlot)
+      </v-btn>
+      <modal :show.sync="modals.Merlot" headerClasses="justify-content-center">
+        <h4 slot="header" class="title title-up">Merlot</h4>
+         <strong>떫지 않고 부드러운 맛 </strong>
+          <p>
+              
+            와인 초심자로서 가볍고 부드러운 와인을 추구한다면 멜롯 품종 와인 추천한다.
 
+            까베르네 소비뇽과 비교해 봤을 때는 포도 과육이 더 많은데 껍질은 얇다. 
+            수분과 당분이 훨씬 많아서 씨앗도 별로 없어서 떫은맛이 덜하다.
+             보통 오랜 숙성을 거치기보다는 짧게 숙성하여도 맛있는 와인이 된다. 
+             와인의 유통기한도 짧기 때문에 빨리 마셔야 한다는 단점이 있다. 만약 와인의 떫은맛이 싫다면 멜롯와인을 강력 추천한다.
+            </p>
+          
+        <template slot="footer">
          
-           
-          </v-card-text>
-        </v-card>
-      </v-tab-item>
-      <v-tab-item>
-        <v-card flat>
-          <v-card-text>
-            <p>
-              적당한 농도감과 부드러움
-            </p>
+          <v-btn type="danger" @click.native="modals.Merlot = false">Close</v-btn>
+        </template>
+      </modal>
+    </div>
+    <div>
+      <v-btn   color="#750049" dark @click.native="modals.Syrah = true">
+       시라(Syrah) / 쉬라즈(Shiraz)
+      </v-btn>
+      <modal :show.sync="modals.Syrah" headerClasses="justify-content-center">
+        <h4 slot="header" class="title title-up">Syrah</h4>
+         <strong>살짝 떫은 맛</strong>
+          <p>
+             
+         쉬라즈라고 불리는 이 품종은 까베르네 소비뇽보다 더 두꺼운 껍질과 더 많은 포도씨를 갖고 있다. 
+         강렬한 탄닌의 맛을 즐기고 싶다면, 혹은 자극적이고 진한 와인을 좋아한다면 쉬라 품종의 와인을 추천한다. 
+         알코올 함량도 높은 편이라서 드라이하다고도 불린다.
+          보통 산도가 낮이라서 신맛은 덜하며 매콤하다고도 사람들이 부를 만큼 자극적인 맛인데 동시에 과일향을 풍기기도 한다. 
+         프랑스에서는 스모크 한 냄새와 허브향이 있다고 하는데 호주의 쉬라는 더 부드럽고 감칠맛이 있다고 한다. 
+          가장 많이 생산하는 국가는 호주이며 저렴하지만 퀄리티가 뛰어난 와인을 대량 생산하고 있다.
 
-            
-          </v-card-text>
-        </v-card>
-      </v-tab-item>
-      <v-tab-item>
-       <v-card flat>
-          <v-card-text>
-            <p>
-              떫지 않고 부드러움
             </p>
+          
+        <template slot="footer">
+         
+          <v-btn type="danger" @click.native="modals.Syrah = false">Close</v-btn>
+        </template>
+      </modal>
+    </div>
+     <div>
+      <v-btn   color="#750049" dark @click.native="modals.Pinot = true">
+       피노 누아(Pinot Noir)
+      </v-btn>
+      <modal :show.sync="modals.Pinot" headerClasses="justify-content-center">
+        <h4 slot="header" class="title title-up">Pinot Noir</h4>
+         <strong>와인 중에 가장 가볍고 떫지않은 맛</strong>
+          <p>
+        비싼 와인이라고 한다면 피노 누아를 많이 생각한다. 
+        피노 누아 품종이 비싼 이유는 이 포도 품종이 아무데서나 잘 적응하지 못하기 때문이다. 
+        주로 서늘한 기후에서 잘 재배되는데 프랑스의 부르고뉴 지방의 피노 누아를 최고로 쳐준다. 
+        피노 누아는 껍질이 얇고 포도 줄기가 적어서 탄닌이 적다. 
+        그만큼 떫은맛은 없어지는데 또 이 맛의 깊이는 라이트 하지 않다. 
+         피노 누아의 Noir는 검은색을 뜻할 만큼 검은색에 가까운 붉은색을 띠고 있다. 
+         이 포도 품종에서는 정말 다양한 과일과 꽃향기, 그리고 짙은 버섯과 흙의 냄새까지 난다고 한다. 
+          부드럽지만 다양한 향과 맛을 즐기고 싶다면 피노 누아 품종의 와인을 추천한다. 
+          혹은 귀중한 사람에게 혹은 특별한 자리를 빛낼 때 피노 누아를 갖고 간다거나 특히 와인을 즐겨 먹는 사람에게 피노 누아를 선물한다면 
+          럭셔리한 선물이 될 것이다.
 
-            <p >
-             그다음으로 레드와인에서 많이 보이는 포도 품종은 바로 멜롯입니다. 자극적인 와인을 좋아하는 제가 그다지 선호하지 않는 품종이기는 하나 와인 초심자로서 가볍고 부드러운 와인을 추구하신다면 멜롯 품종 와인 추천드립니다.
+ 
             </p>
-            <p>
-            까르베네 소비뇽과 비교해 봤을 때는 포도 과육이 더 많은데 껍질은 얇습니다. 수분과 당분이 훨씬 많아서 씨앗도 별로 없습니다. 그러다 보니 떫은맛이 덜합니다. 보통 오랜 숙성을 거치기보다는 짧게 숙성하여도 맛있는 와인이 됩니다. 와인의 유통기한도 짧기 때문에 빨리 마셔야 한다는 단점이 있습니다. 만약 와인의 떫은맛이 싫으신 분들이라면 멜롯와인을 강력 추천드립니다. 
-            </p>
-          </v-card-text>
-        </v-card>
-      </v-tab-item>
-          <v-tab-item>
-       <v-card flat>
-          <v-card-text>
-            <p>
-              호주가 주 품종. 칠레프랑스도 있음. 달지 않고 떫은정도는 상품별로 다름. 살짝 떫은 느낌 있음 스파이시한 향신료 항이 있음
-            </p>
+          
+        <template slot="footer">
+         
+          <v-btn type="danger" @click.native="modals.Pinot = false">Close</v-btn>
+        </template>
+      </modal>
+    </div>
+    <div>
+      <v-btn   color="#750049" dark @click.native="modals.Sangiovese = true">
+       산지오베제(Sangiovese)
+      </v-btn>
+      <modal :show.sync="modals.Sangiovese" headerClasses="justify-content-center">
+        <h4 slot="header" class="title title-up">Sangiovese</h4>
+         <strong>단맛 없고 떫지 않고 피노 누아랑 거의 비슷한 맛</strong>
+          <p>
+     주요 재배지역은 이탈리아 중부 토스카나 주이다. 쥬피터라는 뜻을 지난 이 품종은 이탈리아 최고급 와인을 만드는데 사용된다. 
+적색 루비의 색상을 가지고 있으며, 탄닌은 적당하고, 높은 산도를 가지고 있다. 미디어 바디에서 풀 바디에 이르는 다양한 와인을 만드는데 사용된다. 
 
-            <p >
-             쉬라즈라고 불리는 이 품종은 까르베네 소비뇽보다 더 두꺼운 껍질과 더 많은 포도씨를 갖고 있습니다. 강렬한 탄닌의 맛을 즐기고 싶다면, 혹은 저처럼 자극적이고 진한 와인을 좋아하신다면 쉬라 품종의 와인을 추천드립니다. 알코올 함량도 높은 편이라서 드라이하다고도 불립니다. 보통 산도가 낮이라서 신맛은 덜합니다. 매콤하다고도 사람들이 부를 만큼 자극적인 맛인데 동시에 과일향을 풍기기도 합니다. 산지마다 조금씩 맛이 달라지기도 합니다. 프랑스에서는 스모크 한 냄새와 허브향이 있다고 하는데 호주의 쉬라는 더 부드럽고 감칠맛이 있다고 합니다. 가장 많이 생산하는 국가는 호주이며 저렴하지만 퀄리티가 뛰어난 와인을 대량 생산하고 있습니다. 
+어린 산지오베제는 신선한 딸기 같은 과일향과 약간의 스파이스향이 난다. 배럴에 숙성시켰을 때에는 타르 질과 오크향을 쉽게 느낄 수 있다. 
+산지오베제 포도나무는 일반적으로 매우 활발하고 병에 대한 저항력이 강하지만 열매들을 쉽게 썩게 하는 경향이 있다. 
+체리, 자두향이 대표적이며, 각종 파스타, 피자, 바비큐, 삼겹살, 스테이크 등과 잘 어울린다. 
             </p>
-            
-          </v-card-text>
-        </v-card>
-      </v-tab-item>
-         <v-tab-item>
-       <v-card flat>
-          <v-card-text>
-            <p>
-              와인 중에 가장 가볍고 떫지않은 품종. 멜롯이랑 비슷한데 더 가벼움
+          
+        <template slot="footer">
+         
+          <v-btn type="danger" @click.native="modals.Sangiovese = false">Close</v-btn>
+        </template>
+      </modal>
+    </div>
+     <div>
+      <v-btn   color="#750049" dark @click.native="modals.Tempranillo = true">
+       템프라니요(Tempranillo)
+      </v-btn>
+      <modal :show.sync="modals.Tempranillo" headerClasses="justify-content-center">
+        <h4 slot="header" class="title title-up">Tempranillo</h4>
+         <strong>정말 조금 떫은 맛</strong>
+          <p>
+    스페인 하면 바로 처음 떠오르는 와인 포도 품종이 바로 템프라니요다. 
+템프라니요도 굉장히 강하고 진한 와인의 맛을 낸다. 
+포도 품종의 껍질이 두껍고 색상이 진한 반면에 알코올 함량은 높지 않은 편이다.
+진한 암적색의 색깔에 산딸기와 커피 등의 풍미가 느껴지는 맛이다. 
+탄닌이 다소 떫을 수 있으나 숙성 정도에 따라서 또 좋은 맛이 난다. 
+ 저렴하지만 강렬한 맛을 원한다면 템프라니요 품종 와인을 추천한다.
             </p>
+          
+        <template slot="footer">
+         
+          <v-btn type="danger" @click.native="modals.Tempranillo = false">Close</v-btn>
+        </template>
+      </modal>
+    </div>
+<div>
+      <v-btn   color="#750049" dark @click.native="modals.Grenache = true">
+       그르나슈(Grenache)
+      </v-btn>
+      <modal :show.sync="modals.Grenache" headerClasses="justify-content-center">
+        <h4 slot="header" class="title title-up">Grenache</h4>
+         <strong>바디감이 풍부한 맛</strong>
+          <p>
+    스페인이 원산지이며, 일반적으로 향신료향과 베리향이 나며 입안에서 매우 부드러운 느낌을 준다. 
+껍질이 얇고 탄닌 성분은 풍부하지 못하나 알코올 함유량이 높아 풀바디 스타일을 가진다. 
+그르나슈는 산, 탄닌, 색상이 모자란 경향이 있어 일반적으로 시라, 까리냥, 생소와 같은 품종들과 혼합한다. 
+높은 당분 레벨은 루시용의 천연감미와인을 포함한 주정 강화 와인을 만드는 데 사용된다. 
+오리고기, 소고기, 치즈, 미트소스 스파게티, 토마토 스파게티 등과 잘 어울린다. 
+            </p>
+          
+        <template slot="footer">
+         
+          <v-btn type="danger" @click.native="modals.Grenache = false">Close</v-btn>
+        </template>
+      </modal>
+    </div>
+<div>
+      <v-btn   color="#750049" dark @click.native="modals.Zinfandel = true">
+       진판델(Zinfandel)
+      </v-btn>
+      <modal :show.sync="modals.Zinfandel" headerClasses="justify-content-center">
+        <h4 slot="header" class="title title-up">Zinfandel</h4>
+         <strong>달고 농도감 있는 맛</strong>
+          <p>
+    이탈리아 남부 풀리아 주와 캘리포니아가 주요 지역이며, 캘리포니아에서는 1830년대에 포도원에서 재배된다. 
+풍부한 과일향과 싱싱한 구조, 자연스러운 신맛, 그리고 높은 알코올 함량으로 알려진 인기 있는 캐릴포니아 와인을 만드는데 사용된다. 
+와인 양조자들은 감미로운 화이트 진판델, 보졸레 누보를 생각나게 하는 라이트 바디, 풀바디 드라이 레드, 렝트 하비스트 스위트 디저트 와인, 그리고 포트 와인 스타일을 포함한 광범위한 와인 스타일을 위하여 진판델을 사용한다. 
+ 체리, 베리 계열, 가죽향이 대표적이고, 칠면조, 스테이크, 파스타, 햄, 소시지 등과 잘 어울린다. 
+            </p>
+          
+        <template slot="footer">
+         
+          <v-btn type="danger" @click.native="modals.Zinfandel = false">Close</v-btn>
+        </template>
+      </modal>
+    </div>
+<div>
+      <v-btn   color="#750049" dark @click.native="modals.Nebbiolo = true">
+       네비올로(Nebbiolo)
+      </v-btn>
+      <modal :show.sync="modals.Nebbiolo" headerClasses="justify-content-center">
+        <h4 slot="header" class="title title-up">Nebbiolo</h4>
+         <strong>과일향과 탄닌이 강한 맛</strong>
+          <p>
+   이탈리아 북서 피에몬테 지역에서 가장 중요한 와인을 생산하는 품종이다. 
+네비올로란 뜻은 이탈리아어로 안개란 뜻이며, 껍질이 두껍고 탄닌과 알코올이 풍부하여 와인의 단단한 구조와 남성적인 묵직함을 주며, 장기 숙성이 가능하다. 
+노균병에 약한 편이고, 품종이 아주 섬세하기 때문에 양조시 항상 주의 깊게 살펴야 한다. 
+바롤로, 바르바레스코와 같은 최고의 와인을 만드는 데 사용된다. 
+엷은 색상의 레드 와인들은 흥미를 자아내는 타르와 장미향과 함께 젊음의 힘찬 탄닌을 얻을 수 있다. 
+와인들이 숙성되면, 복잡한 향과 매우 독특하고 과일, 꽃향, 스파이시한 특징을 느낄 수 있다. 
+이들 와인들의 탄닌이 유순하게 되려면 수년간의 숙성이 소요된다. 
+훈제연어, 소고기, 양고기 등과 잘 어울리며, 특히 훈제, 숯불요리와 매우 좋은 궁합이다. 
 
-            <p >
-             비싼 와인이라고 한다면 피노누아를 많이 생각합니다. 피노누아 품종이 비싼 이유는 이 포도 품종이 아무데서나 잘 적응하지 못하기 때문입니다. 주로 서늘한 기후에서 잘 재배되는데 프랑스의 부르고뉴 지방의 피노누아를 최고로 쳐주고는 합니다. 피노누아는 껍질이 얇고 포도 줄기가 적어서 탄닌이 적습니다.
+ 
             </p>
-            <p>
-            그만큼 떫은맛은 없어지는데 또 이 맛의 깊이는 라이트 하지 않습니다. 피노 누아의 Noir는 검은색을 뜻할 만큼 검은색에 가까운 붉은색을 띠고 있습니다. 이 포도 품종에서는 정말 다양한 과일과 꽃향기, 그리고 짙은 버섯과 흙의 냄새까지 난다고 합니다. 부드럽지만 다양한 향과 맛을 즐기고 싶으신 분들께 피노누아 품종의 와인 추천드립니다. 혹은 어딘가 귀중한 분께 혹은 특별한 자리를 빛낼 때 피노누아를 갖고 간다거나 특히 와인을 즐겨 드시는 분들께 피노누아를 선물한다면 럭셔리한 선물이 될 것입니다. 
-            </p>
-          </v-card-text>
-        </v-card>
-      </v-tab-item>
-       <v-tab-item>
-       <v-card flat>
-          <v-card-text>
-            <p>
-              단맛 없고 떫지 않고 피노누아랑 거의 비슷. 과실향이 정말 풍부하며, 목넘김이 부드러움
-            </p>
+          
+        <template slot="footer">
+         
+          <v-btn type="danger" @click.native="modals.Nebbiolo = false">Close</v-btn>
+        </template>
+      </modal>
+    </div>
+    <div>
+      <v-btn   color="#750049" dark @click.native="modals.Barbera = true">
+       바르베라(Barbera)
+      </v-btn>
+      <modal :show.sync="modals.Barbera" headerClasses="justify-content-center">
+        <h4 slot="header" class="title title-up">Barbera</h4>
+         <strong>미디엄 바디에 과일 풍미가 풍부한 맛</strong>
+          <p>
+   13세기부터 이탈리아에 알려져있으며, 북서 이탈리아는 바르베라를 위하여 가장 중요한 지역이다. 이탈리아에서 가장 많이 혹은 두 번째로 많이 재배되는 품종이다. 
+바르베라의 산도는 와인의 신선함을 주기 위하여 다른 품종들과 혼합한다. 
+수확량이 좋고, 깊은 색상, 낮은 탄닌과 따뜻한 기후의 레드 품종과는 다르게 높은 레벨의 산도를 준다. 
+오랜 숙성을 거친 와인은 과일향이 강렬하고, 탄닌의 함유량을 향상시켜 매우 감칠맛이 난다. 
+많은 생산자들이 구운 오크통의 향을 첨가하며, 전통적으로 슬로베니아산 오크통에서 몇 년간 숙성되고 있으나, 최근에는 가격이 비싼 프랑스산 오크통에서 숙성시키는 경향이 있다.
 
-            <p >
-             비싼 와인이라고 한다면 피노누아를 많이 생각합니다. 피노누아 품종이 비싼 이유는 이 포도 품종이 아무데서나 잘 적응하지 못하기 때문입니다. 주로 서늘한 기후에서 잘 재배되는데 프랑스의 부르고뉴 지방의 피노누아를 최고로 쳐주고는 합니다. 피노누아는 껍질이 얇고 포도 줄기가 적어서 탄닌이 적습니다.
-            </p>
-            <p>
-            그만큼 떫은맛은 없어지는데 또 이 맛의 깊이는 라이트 하지 않습니다. 피노 누아의 Noir는 검은색을 뜻할 만큼 검은색에 가까운 붉은색을 띠고 있습니다. 이 포도 품종에서는 정말 다양한 과일과 꽃향기, 그리고 짙은 버섯과 흙의 냄새까지 난다고 합니다. 부드럽지만 다양한 향과 맛을 즐기고 싶으신 분들께 피노누아 품종의 와인 추천드립니다. 혹은 어딘가 귀중한 분께 혹은 특별한 자리를 빛낼 때 피노누아를 갖고 간다거나 특히 와인을 즐겨 드시는 분들께 피노누아를 선물한다면 럭셔리한 선물이 될 것입니다. 
-            </p>
-          </v-card-text>
-        </v-card>
-      </v-tab-item>
-       <v-tab-item>
-       <v-card flat>
-          <v-card-text>
-            <p>
-              정말 조금 떫음
-            </p>
+바르베라품종은 이탈리아의 다른 많은 DOC 와인들을 만드는 데 허가된 품종이다. 
 
-            <p >
-             스페인 하면 바로 처음 떠오르는 와인 포도 품종이 바로 템프라니요입니다.. 이 중에서 Tempranilo 템프라니요라는 품종을 볼 수 있을 겁니다. 이 템프라니요도 굉장히 강하고 진한 와인의 맛을 냅니다. 포도 품종의 껍질이 두껍고 색상이 진한 반면에 알코올 함량은 높지 않은 편입니다. 진한 암적색의 색깔에 산딸기와 커피 등의 풍미가 느껴지는 맛입니다. 탄닌이 다소 떫을 수 있으나 숙성 정도에 따라서 또 좋은 맛이 납니다. 요즘 저는 저렴한 와인을 마시기 때문에 저렴하지만 강렬한 맛을 원하실 때 저는 템프라니요 품종 와인을 추천드립니다. 
+따뜻한 기후를 좋아하고, 많은 수확량을 가져다주는 품종이다. 
             </p>
-           
-          </v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs>
-    </v-menu>
+          
+        <template slot="footer">
+         
+          <v-btn type="danger" @click.native="modals.Barbera = false">Close</v-btn>
+        </template>
+      </modal>
+    </div>
       </v-col>
        <v-col cols="6">
 <v-menu offset-y :close-on-content-click="false">
@@ -496,6 +568,7 @@ export default {
         hover: false,
 
 
+
         selectedItem: 1,
       items: [
           "와이너리","생산지", "포도 품종", "빈티지"
@@ -509,6 +582,17 @@ export default {
             cabernet:false,
             malbec:false,
             Carmenere:false,
+            Merlot:false,
+            Syrah:false,
+            Pinot:false,
+            Sangiovese:false,
+            Tempranillo:false,
+            Grenache:false,
+            Zinfandel:false,
+            Nebbiolo:false,
+            Barbera:false,
+
+
           }
       }
   }
