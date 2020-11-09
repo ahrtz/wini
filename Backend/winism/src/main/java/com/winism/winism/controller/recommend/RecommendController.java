3d9eaa1572b3a61b177f70b3dfb99cea58ebec96
이원호 <dldnjsgh71@naver.com>
2020-11-09 17:07:14 +0900

    [feat] modify python file

--- Backend/winism/src/main/java/com/winism/winism/controller/recommend/RecommendController.java
+++ Backend/winism/src/main/java/com/winism/winism/controller/recommend/RecommendController.java
@@ -15,7 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = { "*" },maxAge = 6000)
@@ -27,8 +29,8 @@ public class RecommendController {
     @Autowired
     WineService wineservice;
 
-    @PostMapping("/recommend/contents")
-    public ResponseEntity<List<Object>> recommendbycontents(String userid){
+    @GetMapping("/recommend/contents")
+    public ResponseEntity<List<Object>> recommendbycontents(@RequestParam(required = false)String userid){
 
         List<Object> result = new ArrayList<Object>();
         System.out.println(1);
