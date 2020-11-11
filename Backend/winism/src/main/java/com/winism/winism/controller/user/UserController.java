aff823f8099a7fcb0ce4a00a64d5158fc8f99bb5
woqls8449 <woqls8449@gmail.com>
2020-11-11 10:05:32 +0900

    [feat] Add cors allow

--- Backend/winism/src/main/java/com/winism/winism/controller/user/UserController.java
+++ Backend/winism/src/main/java/com/winism/winism/controller/user/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = { "*" })
 @Api(tags = {"2. User"})
 @RequiredArgsConstructor
 @RestController
